281fc4d11a8528ce88fbdb54c4f59b7658088adc
Atanu Kumar Panja <atanu.csmcri22a@acsir.res.in>
2024-01-07 12:17:38 +0530

    arranged_dataset_programming_2.R

--- arranged_dataset_programming_2.R
+++ arranged_dataset_programming_2.R
@@ -4,6 +4,21 @@ library(dplyr)
 # Load the data from the CSV file
 data <- read.csv("GLODAPv2.2022_Merged_Master_File.csv")
 
+# Assuming 'data' is your dataframe
+data <- data %>%
+  mutate(
+    G2nitrate = ifelse(G2nitrate == -9999, "", G2nitrate),
+    G2oxygen = ifelse(G2oxygen == -9999, "", G2oxygen),
+    G2silicate = ifelse(G2silicate == -9999, "", G2silicate),
+    G2phosphate = ifelse(G2phosphate == -9999, "", G2phosphate),
+    G2fco2 = ifelse(G2fco2 == -9999, "", G2fco2),
+    G2tco2 = ifelse(G2tco2 == -9999, "", G2tco2),
+    G2nitrite = ifelse(G2nitrite == -9999, "", G2nitrite),
+    G2doc = ifelse(G2doc == -9999, "", G2doc),
+    G2don = ifelse(G2don == -9999, "", G2don),
+    G2toc = ifelse(G2toc == -9999, "", G2toc)
+  )
+
 # Filter out rows with -9999 values in any of the specified columns
 filtered_data <- data %>%
   filter(G2nitrate != -9999,
